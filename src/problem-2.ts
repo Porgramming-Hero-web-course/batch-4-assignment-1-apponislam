f673fec279444e367cf4736b35a51eacaae8744c
apponislame <11appon11@gmail.com>
2024-11-06 18:46:29 +0600

    Second Problem Solved

---
+++ src/problem-2.ts
@@ -0,0 +1,13 @@
+function removeDuplicates(numbers: number[]): number[] {
+    const newArray: number[] = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (!newArray.includes(numbers[i])) {
+            newArray.push(numbers[i]);
+        }
+    }
+
+    return newArray;
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5, 1, 6, 8, 9, 9, 9, 10, 10]));
