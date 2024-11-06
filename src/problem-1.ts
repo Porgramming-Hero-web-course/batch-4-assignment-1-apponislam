92487ef341e957f21227731ee9b207bba9251836
apponislame <11appon11@gmail.com>
2024-11-06 18:38:53 +0600

    first problem solved

---
+++ src/problem-1.ts
@@ -0,0 +1,11 @@
+function sumArray(numbers: number[]): number {
+    let total = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+
+    return total;
+}
+
+console.log(sumArray([1, 2, 3, 4, 6, 4]));
